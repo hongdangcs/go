005703d34d5cf83338d6b0028df86b5bc0cb33ab
hongdangcseiu <hongdang2k1@gmail.com>
2022-08-09 22:06:41 +0700

    if statements

--- src/golang-book/chap2/helloworld.go
+++ src/golang-book/chap2/helloworld.go
@@ -3,10 +3,11 @@ import "fmt"
 func main() {
 	i := 1
 	for i<10{
-		fmt.Println(i)
+		if(i%2==0){
+			fmt.Println(i, " even")
+		} else {
+			fmt.Println(i, " odd")
+		}
 		i++
-	}
-	for j:=0; j<10; j++{
-		fmt.Println(j)
 	}
 }
