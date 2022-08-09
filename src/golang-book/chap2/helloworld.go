706717d1c2bf19f05592c61beef55a73a02504f5
hongdangcseiu <hongdang2k1@gmail.com>
2022-08-09 22:15:42 +0700

    switch case

--- src/golang-book/chap2/helloworld.go
+++ src/golang-book/chap2/helloworld.go
@@ -2,12 +2,11 @@ package main
 import "fmt"
 func main() {
 	i := 1
-	for i<10{
-		if(i%2==0){
-			fmt.Println(i, " even")
-		} else {
-			fmt.Println(i, " odd")
-		}
-		i++
+	switch i{
+		case 1: fmt.Println("one")
+		case 2: fmt.Println("two")
+		case 3: fmt.Println("three")
+		case 4: fmt.Println("for")
+		default: fmt.Println("idk")
 	}
 }
